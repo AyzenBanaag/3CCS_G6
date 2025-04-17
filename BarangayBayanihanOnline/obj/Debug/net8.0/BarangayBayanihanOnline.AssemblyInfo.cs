c6ef66e118a9518de32cd17d53dab4a3d245679d
Jayssen <nessyajgaanab@gmail.com>
2025-04-17 23:07:18 +0800

    LATEST

--- BarangayBayanihanOnline/obj/Debug/net8.0/BarangayBayanihanOnline.AssemblyInfo.cs
+++ BarangayBayanihanOnline/obj/Debug/net8.0/BarangayBayanihanOnline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarangayBayanihanOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f32f34bb4b4e9c7ab4800ac36dea0c503a2667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7562d8f9f526445e75342047c701981c83a552")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarangayBayanihanOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarangayBayanihanOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
